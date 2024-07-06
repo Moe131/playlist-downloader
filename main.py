fbbdcd173cabedf73ee4908669ea6f48ef67c8d5
Mohammad Mirzaei <mirzaei_1378@yahoo.com>
2024-07-06 00:52:57 -0700

    chromedriver added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service(executable_path="./chromedriver")
+driver = webdriver.Chrome(service=service)
+
 def main():
-    pass
+    driver.get("https://google.com")
+    time.sleep(10)
+    driver.quit()
 
 if __name__ == "__main__":
     main()
